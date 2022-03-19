3bfb56878f534d588de1f1c7334c5a5f2667a3ba
FatimaGFloresF <ff21017@ues.edu.sv>
2022-03-18 18:50:53 -0600

    Commit Inicial

---
+++ src/segundo_ejercicio_progra/Segundo_Ejercicio_Progra.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package segundo_ejercicio_progra;
+
+/**
+ *
+ * @author tejad
+ */
+public class Segundo_Ejercicio_Progra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
